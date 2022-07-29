a0ea94124c4eb7fc105b8187aefdf109542edcbd
rwtrahul10 <Rohitsinghrawat8@gmail.com>
2022-07-29 16:18:02 +0530

    first commit

--- attendance.js
+++ attendance.js
@@ -1,17 +1,23 @@
-function checkAttendance(maxAttendence){
-    console.log("\nMinium lectures needed to attend to achieve 75% attendance is \t"+(3*maxAttendence/4)+"\t"+String.fromCharCode(9996)+"\n" );
+function checkAttendance(maxAttendence) {
+  console.log(
+    "\nMinium lectures needed to attend to achieve 75% attendance is \t" +
+      (3 * maxAttendence) / 4 +
+      "\t" +
+      String.fromCharCode(9996) +
+      "\n"
+  );
 
-    // string.charCodeAt(97);
-var x= 'a';
-let i=0;
-while(i<10){
-let char = String.fromCharCode(i);
-console.log("ASCII value of "+i+"  "+char);
+  // string.charCodeAt(97);
+  var x = "a";
+  let i = 0;
+  while (i < 10) {
+    let char = String.fromCharCode(i);
+    console.log("ASCII value of " + i + "  " + char);
 
-i++;
-var g= 77;
-}
-console.log(g);
+    i++;
+    var g = 77;
+  }
+  console.log(g);
 }
 checkAttendance(80);
 
